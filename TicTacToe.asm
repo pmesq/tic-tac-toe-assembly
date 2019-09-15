e4cf6854c3cf40de18df7d20b6f94c99e2bd0b75
Pedro César (Alface) <pedrocesar1410@gmail.com>
2019-09-15 19:32:57 -0300

    Update TicTacToe.asm

--- TicTacToe.asm
+++ TicTacToe.asm
@@ -6,11 +6,9 @@ ORG 100H
 .DATA
 
 board               DB " | | ", 10, 13, " | | ", 10, 13, " | | ", 10, 13, 10, 13, "$"
-    
 msgTitle            DB "Tic Tac Toe", 10, 13, 10, 13, "$"
 msgSquaresNumbers   DB "Squares numbers:", 10, 13, "1|2|3", 10, 13, "4|5|6", 10, 13, "7|8|9", 10, 13, 10, 13, "$"
 msgBoard            DB "Current board:", 10, 13, "$"
-msgMarkError        DB "Unavailable square. Try again", 10, 13, "$"
 msgEnterSquare      DB "Player * plays now", 10, 13, "Enter the square number: $"
 msgPlayerWins       DB "Player * wins!", 10, 13, "$"
 msgDraw             DB "Draw!$"
